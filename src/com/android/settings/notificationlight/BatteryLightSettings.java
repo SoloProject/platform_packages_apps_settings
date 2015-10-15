655568e4ba021f9d53760a883e1706a6bc868d9b
mike <mikew8978@gmail.com>
2015-10-14 23:54:24 -0400

    Add missing drawable

--- src/com/android/settings/notificationlight/BatteryLightSettings.java
+++ src/com/android/settings/notificationlight/BatteryLightSettings.java
@@ -121,7 +121,7 @@ public class BatteryLightSettings extends SettingsPreferenceFragment implements
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.add(0, MENU_RESET, 0, R.string.profile_reset_title)
-                .setIcon(R.drawable.ic_settings_backup)
+                .setIcon(R.drawable.ic_settings_backup_restore)
                 .setAlphabeticShortcut('r')
                 .setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
     }
